59086ff384034dc23b353ecaa7f66222a3905a4f
Abdelrahman atef Hassan <102483897+AbdoMan19@users.noreply.github.com>
2025-03-20 08:30:15 +0200

    add generic response model

---
+++ SnapNFix.Application/Utilities/Constants.cs
@@ -0,0 +1,7 @@
+namespace SnapNFix.Application.Utilities;
+
+public static class Constants
+{
+    public static string SuccessMessage { get; set; } = "The operation was completed successfully";
+    public static string FailureMessage { get; set; } = "Failure";
+}
